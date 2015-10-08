ff925adbb106c07469ee2babaa3aa70d5d833eb4
nickyska <nickyska@gmail.com>
2015-10-07 23:17:40 -0400

    add custom filter for adapter

--- app/src/main/java/com/android/homify/activities/MainActivity.java
+++ app/src/main/java/com/android/homify/activities/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.android.homify.R;
 import com.android.homify.activities.adapter.PreferenceArrayAdapter;
@@ -23,19 +25,26 @@ public class MainActivity extends Activity {
     public static final String USER_PREFERENCE = "user-preference";
     private ListView mainListView = null;
     private List<Preference> preferences = null;
-    private ArrayAdapter<Preference> listAdapter = null;
+    private PreferenceArrayAdapter listAdapter = null;
 
     private PreferencesDaoImpl preferencesDao;
+
+    private SearchView search;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // search = (SearchView) findViewById(R.menu.menu);
 
         this.preferencesDao = new PreferencesDaoImpl(this);
         this.preferencesDao.open(getResources());
 
         mainListView = (ListView) findViewById(R.id.mainListView);
+
+        //setSearchViewListener(this.search);
+
+
 
         // When item is tapped, toggle checked properties of CheckBox and
         // Preference.
@@ -60,20 +69,66 @@ public class MainActivity extends Activity {
         mainListView.setAdapter(listAdapter);
     }
 
+    private void setSearchViewListener(SearchView searchView) {
+
+        search.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), String.valueOf(hasFocus),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //*** setOnQueryTextListener ***
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                listAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+    }
+
     private List<Preference> getSavedPreferences() {
 
         List<Preference> preferencesByType = this.preferencesDao.getPreferencesByType(USER_PREFERENCE);
         return preferencesByType;
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_my, menu);
+        inflater.inflate(R.menu.menu, menu);
 
+        this.search = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        setSearchViewListener(this.search);
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                // search action
+                return true;
+        }
+
+        return false;
+    }
 
     public Object onRetainNonConfigurationInstance() {
         return preferences;
--- app/src/main/java/com/android/homify/activities/adapter/PreferenceArrayAdapter.java
+++ app/src/main/java/com/android/homify/activities/adapter/PreferenceArrayAdapter.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.android.homify.R;
@@ -13,23 +15,30 @@ import com.android.homify.db.PreferencesDaoImpl;
 import com.android.homify.model.Preference;
 import com.android.homify.model.PreferenceViewHolder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 /**
  * Created by nickyska on 9/30/2015.
  */
-public class PreferenceArrayAdapter extends ArrayAdapter<Preference> {
+public class PreferenceArrayAdapter extends ArrayAdapter<Preference> implements Filterable {
 
     private LayoutInflater inflater;
 
     private PreferencesDaoImpl preferencesDao;
+
+    private ArrayList<Preference> originalPreferences;
+    private ArrayList<Preference> filteredPreferences;
+    private Filter filter;
 
     public PreferenceArrayAdapter(Context context, List<Preference> preferencesList, PreferencesDaoImpl preferencesDao) {
         super(context, R.layout.simplerow, R.id.rowTextView, preferencesList);
         //Cache the LayoutInflate to avoid asking for a new one each time.
         inflater = LayoutInflater.from(context);
         this.preferencesDao = preferencesDao;
+        this.originalPreferences = new ArrayList<Preference>(preferencesList);
+        this.filteredPreferences = new ArrayList<Preference>(preferencesList);
     }
 
     @Override
@@ -60,8 +69,7 @@ public class PreferenceArrayAdapter extends ArrayAdapter<Preference> {
         // Re-use existing row view
         else {
 
-            PreferenceViewHolder viewHolder = (PreferenceViewHolder) convertView
-                    .getTag();
+            PreferenceViewHolder viewHolder = (PreferenceViewHolder) convertView.getTag();
             checkBox = viewHolder.getCheckBox();
             textView = viewHolder.getTextView();
         }
@@ -75,4 +83,51 @@ public class PreferenceArrayAdapter extends ArrayAdapter<Preference> {
         return convertView;
     }
 
+    @Override
+    public Filter getFilter() {
+
+        if (filter == null)
+            filter = new PreferenceFilter();
+
+        return filter;
+    }
+
+    private class PreferenceFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            String prefix = constraint.toString().toLowerCase();
+
+            if (prefix == null || prefix.length() == 0) {
+                ArrayList<Preference> list = new ArrayList<Preference>(originalPreferences);
+                results.values = list;
+                results.count = list.size();
+            } else {
+                final ArrayList<Preference> originalListOfPreferences = new ArrayList<Preference>(originalPreferences);
+                final ArrayList<Preference> newListOfPreferences = new ArrayList<Preference>();
+
+                for (final Preference pre : originalListOfPreferences) {
+                    if (pre.getName().toLowerCase().contains(prefix)) {
+                        newListOfPreferences.add(pre);
+                    }
+                }
+                results.values = newListOfPreferences;
+                results.count = newListOfPreferences.size();
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            filteredPreferences = (ArrayList<Preference>) results.values;
+
+            clear();
+            for (Preference pref : filteredPreferences) {
+                add(pref);
+            }
+
+        }
+    }
 }
