bf89048a865df699f7050d10c32b9086d538c1f7
nickyska <nickyska@gmail.com>
2015-10-17 19:58:16 -0400

    add autocomplete for address

--- app/src/main/java/com/android/homify/activities/UnitPreferenceActivity.java
+++ app/src/main/java/com/android/homify/activities/UnitPreferenceActivity.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import com.android.homify.R;
+import com.android.homify.activities.adapter.PlaceAutocompleteAdapter;
 import com.android.homify.model.Preference;
 import com.android.homify.model.PreferenceBuilder;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,12 +42,99 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
     private final static int FATEST_INTERVAL = 5000; // 5 sec
     private final static int DISPLACEMENT = 10; // 10 meters
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private static final LatLngBounds BOUNDS_GREATER_SYDNEY = new LatLngBounds(
+            new LatLng(-34.041458, 150.790100), new LatLng(-33.682247, 151.383362));
     private Location mLastLocation;
     // Google client to interact with Google API
     private GoogleApiClient mGoogleApiClient;
     // boolean flag to toggle periodic location updates
     private boolean mRequestingLocationUpdates = false;
+
+
+    /////////////
     private LocationRequest mLocationRequest;
+    /**
+     * GoogleApiClient wraps our service connection to Google Play Services and provides access
+     * to the user's sign in state as well as the Google's APIs.
+     */
+
+    private PlaceAutocompleteAdapter mAdapter;
+    private AutoCompleteTextView mAutocompleteView;
+
+    /////
+    /**
+     * Callback for results from a Places Geo Data API query that shows the first place result in
+     * the details view on screen.
+     */
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+
+//            // Format details of the place for display and show it in a TextView.
+//            mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(),
+//                    place.getId(), place.getAddress(), place.getPhoneNumber(),
+//                    place.getWebsiteUri()));
+
+//            // Display the third party attributions if set.
+//            final CharSequence thirdPartyAttribution = places.getAttributions();
+//            if (thirdPartyAttribution == null) {
+//                mPlaceDetailsAttribution.setVisibility(View.GONE);
+//            } else {
+//                mPlaceDetailsAttribution.setVisibility(View.VISIBLE);
+//                mPlaceDetailsAttribution.setText(Html.fromHtml(thirdPartyAttribution.toString()));
+//            }
+
+            Log.i(TAG, "Place details received: " + place.getName());
+
+            places.release();
+        }
+    };
+    /**
+     * Listener that handles selections from suggestions from the AutoCompleteTextView that
+     * displays Place suggestions.
+     * Gets the place id of the selected item and issues a request to the Places Geo Data API
+     * to retrieve more details about the place.
+     *
+     * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
+     * String...)
+     */
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+            final PlaceAutocompleteAdapter.PlaceAutocomplete item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId().toString();
+            final CharSequence primaryText = "";
+
+            Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+        }
+    };
 
     @Override
     protected int getActionBarLayoutId() {
@@ -48,17 +145,44 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View actionBarView = getActionBarView(this);
-        actionBarView.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                displayLocation();
-            }
-        });
+        // Construct a GoogleApiClient for the {@link Places#GEO_DATA_API} using AutoManage
+        // functionality, which automatically sets up the API client to handle Activity lifecycle
+        // events. If your activity does not extend FragmentActivity, make sure to call connect()
+        // and disconnect() explicitly.
+        buildGoogleApiClient();
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                //.enableAutoManage(this, 0 /* clientId */, this)
+//                .addApi(Places.GEO_DATA_API)
+//                .build();
+
+        //setContentView(R.layout.activity_main);
+
+        // Retrieve the AutoCompleteTextView that will display Place suggestions.
+        mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.autocomplete_places);
+
+        // Register a listener that receives callbacks when a suggestion has been selected
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+
+        // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
+        // the entire world.
+        List<Integer> filterTypes = new ArrayList<Integer>();
+        filterTypes.add(Place.TYPE_STREET_ADDRESS);
+        mAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, BOUNDS_GREATER_SYDNEY,
+                null);
+        mAutocompleteView.setAdapter(mAdapter);
+
+//        View actionBarView = getActionBarView(this);
+//        actionBarView.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                displayLocation();
+//            }
+//        });
 
     }
-
 
     @Override
     public List<Preference> getPreferencesList() {
@@ -73,27 +197,44 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
         return list;
     }
 
-
     /**
      * Method to display the location on UI
      */
     private void displayLocation() {
 
-        mLastLocation = LocationServices.FusedLocationApi
-                .getLastLocation(mGoogleApiClient);
-
-        if (mLastLocation != null) {
-            double latitude = mLastLocation.getLatitude();
-            double longitude = mLastLocation.getLongitude();
-
-            //lblLocation.setText(latitude + ", " + longitude);
-            Toast.makeText(getBaseContext(), latitude + ", " + longitude, Toast.LENGTH_SHORT).show();
-
-        } else {
-            Toast.makeText(getBaseContext(), "(Couldn't get the location. Make sure location is enabled on the device)", Toast.LENGTH_SHORT).show();
-//            lblLocation
-//                    .setText();
-        }
+//        mLastLocation = LocationServices.FusedLocationApi
+//                .getLastLocation(mGoogleApiClient);
+//
+//        if (mLastLocation != null) {
+//            double latitude = mLastLocation.getLatitude();
+//            double longitude = mLastLocation.getLongitude();
+//
+//            //lblLocation.setText(latitude + ", " + longitude);
+//            Toast.makeText(getBaseContext(), latitude + ", " + longitude, Toast.LENGTH_SHORT).show();
+//
+//        } else {
+//            Toast.makeText(getBaseContext(), "(Couldn't get the location. Make sure location is enabled on the device)", Toast.LENGTH_SHORT).show();
+////            lblLocation
+////                    .setText();
+//        }
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                if (!likelyPlaces.getStatus().isSuccess()) {
+                    // Request did not complete successfully
+                    Log.e(TAG, "Place query did not complete. Error: " + likelyPlaces.getStatus().toString());
+                    likelyPlaces.release();
+                    return;
+                }
+                String placeName = String.format("%s", likelyPlaces.get(0).getPlace().getName());
+                String placeAttributuion = String.format("%s", likelyPlaces.get(0).getPlace().getAddress());
+                //mPlaceDetailsText.setText(placeName);
+                // mPlaceDetailsAttribution.setText(placeAttributuion);
+                likelyPlaces.release();
+            }
+        });
     }
 
     /**
@@ -103,7 +244,8 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi(Places.GEO_DATA_API).build();
     }
 
     /**
@@ -145,11 +287,11 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
     /**
      * Google api callback methods
      */
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
-                + result.getErrorCode());
-    }
+//    @Override
+//    public void onConnectionFailed(ConnectionResult result) {
+//        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+//                + result.getErrorCode());
+//    }
 
     @Override
     public void onConnected(Bundle arg0) {
@@ -157,6 +299,9 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
         // Once connected with google api, get the location
         displayLocation();
     }
+
+
+    //////
 
     @Override
     public void onConnectionSuspended(int arg0) {
@@ -185,4 +330,35 @@ public class UnitPreferenceActivity extends AbstarctPreferenceActivity
         }
 
     }
+
+//    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+//                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+//        //Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
+//               // websiteUri));
+//        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+//                websiteUri));
+//
+//    }
+
+    /**
+     * Called when the Activity could not connect to Google Play services and the auto manager
+     * could resolve the c automatically.
+     * In this case the API is not available and notify the user.
+     *
+     * @param connectionResult can be inspected to determine the cause of the failure
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.e(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+
+        // TODO(Developer): Check error code and notify the user of error state and resolution.
+        Toast.makeText(this,
+                "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+    ///
 }
